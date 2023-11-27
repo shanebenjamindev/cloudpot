375e1f947ba48f36675aa58e51c73bfbe796ae07
Your Name <you@example.com>
2023-11-28 02:09:05 +0700

    added images

--- src/pages/Menu/Drink/index.jsx
+++ src/pages/Menu/Drink/index.jsx
@@ -10,11 +10,15 @@ export default function Drink() {
       <h1 className='section__Title'>Drinks</h1>
       <hr className="w-75" style={{ border: "1px solid var(--secondary-color)" }} />
       <div className='drink-list d-md-flex'>
-        <div className='col-md-6'>
-          <h1 className='section__Title'>Drinks</h1>
-          <hr className="w-75" style={{ border: "1px solid var(--secondary-color)" }} />
-
-          
+        <div className='drink-item' data-aos="fade-up"
+          data-aos-duration="1500">
+          <img src={drink3} />
+        </div>
+        <div className='drink-item' data-aos="fade-right" data-aos-delay={1000} data-aos-duration="1000">
+          <img src={drink} />
+        </div>
+        <div className='drink-item' data-aos="fade-right" data-aos-delay={1500} data-aos-duration="500">
+          <img src={drink2} />
         </div>
       </div>
 
