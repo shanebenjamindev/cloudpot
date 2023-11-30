c117964536d613efa2d7ecd191a2814a5c2e6821
giang <you@example.com>
2023-12-01 06:05:53 +0700

    updated menu

--- src/pages/Menu/ALC/index.jsx
+++ src/pages/Menu/ALC/index.jsx
@@ -1,71 +1,97 @@
 import { useState } from 'react';
 import menuJson from '../../../../public/images/cloudpot-menu/menuJson.json';
-import './ALC.css'
-
+import './ALC.css';
 
 const ALC = () => {
-    const data = menuJson;
-    const [searchTerm, setSearchTerm] = useState('');
+  const data = menuJson;
+  const [searchTerm, setSearchTerm] = useState('');
 
-    const filteredData = data.map((group, groupIndex) => {
-        const filteredGroup = {
-            [Object.keys(group)[0]]: group[Object.keys(group)[0]].filter((item) =>
-                item.Tênmón.toLowerCase().includes(searchTerm.toLowerCase())
-            ),
-        };
-        return filteredGroup;
-    });
+  // Group items by "Nhóm"
+  const groupedData = data.reduce((result, item) => {
+    const groupName = item.Nhóm;
+    if (!result[groupName]) {
+      result[groupName] = [];
+    }
+    result[groupName].push(item);
+    return result;
+  }, {});
 
-    return (
-        <section id='section__ALC' className="container mt-5">
-            <div className='d-md-flex  justify-content-between'>
-                <h3 className='section__Title col-md-7'>Menu ALC</h3>
-                <input
-                    type="text"
-                    className='form-control col-md-5'
-                    placeholder="Search..."
-                    value={searchTerm}
-                    onChange={(e) => setSearchTerm(e.target.value)}
-                />
+  // Filter and display grouped data
+  const filteredData = Object.keys(groupedData).map((groupName, groupIndex) => {
+    return {
+      groupName,
+      items: groupedData[groupName].filter((item) =>
+        item['Tên món'].toLowerCase().includes(searchTerm.toLowerCase())
+      ),
+    };
+  });
+
+  // Calculate number of tables in each column
+  const tablesPerColumn = Math.ceil(filteredData.length / 2);
+
+  return (
+    <section id='section__ALC' className='w-75 mx-auto mt-5'>
+      <div className='d-md-flex justify-content-between'>
+        <h3 className='section__Title col-md-7'>Menu ALC</h3>
+        <input
+          type='text'
+          className='form-control col-md-5'
+          placeholder='Search...'
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+      <hr className='w-25' style={{ border: '1px solid var(--secondary-color)' }} />
+
+      <div className='row'>
+        {filteredData.map((group, groupIndex) => (
+          <div key={groupIndex} className={`col-md-${tablesPerColumn > 1 ? '6' : '12'}`}>
+            <h4 className='table-title'>{group.groupName}</h4>
+            <div className='table-responsive bg-white'>
+              <table className='table table-bordered table-striped'>
+                <thead className='thead text-white bg-success'>
+                  <tr>
+                    <th>STT</th>
+                    <th>Tên món</th>
+                    <th>ĐL tổng (gram)</th>
+                    <th>Giá bán (HCM)</th>
+                    <th>Nước chấm</th>
+                    <th>Rau ăn kèm</th>
+                    <th>Note</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {group.items.map((item, itemIndex) => (
+                    <tr key={itemIndex}>
+                      <td>{itemIndex + 1}</td>
+                      <td>{item['Tên món']}</td>
+                      <td>{item['ĐL']} gram</td>
+                      <td>{item['Giá bán HCM']} VND</td>
+                      <td>
+                        {item['Nước chấm'] !== null ? (
+                          <div className='text-success'>{item['Nước chấm']}</div>
+                        ) : (
+                          <div className='text-danger'>none</div>
+                        )}
+                      </td>
+                      <td>
+                        {item['Rau ăn kèm'] !== null ? (
+                          <div className='text-success'>{item['Rau ăn kèm']}</div>
+                        ) : (
+                          <div className='text-danger'>none</div>
+                        )}
+                      </td>
+                      <td>{item['Note']}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
             </div>
-            <hr className="w-25" style={{ border: "1px solid var(--secondary-color)" }} />
-
-
-            {filteredData.map((group, groupIndex) => (
-                <div key={groupIndex}>
-                    <h4 className='table-title'>{Object.keys(group)[0]}</h4>
-                    <div className="table-responsive bg-white">
-                        <table className="table table-bordered table-striped">
-                            <thead className="thead text-white bg-success">
-                                <tr>
-                                    <th>STT</th>
-                                    <th>Tên món</th>
-                                    <th>Thời gian chín</th>
-                                    <th>ĐL tổng (gram)</th>
-                                    <th>Nước chấm</th>
-                                    <th>Rau ăn kèm</th>
-                                    <th>Giá bán (HCM)</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {group[Object.keys(group)[0]].map((item, itemIndex) => (
-                                    <tr key={itemIndex}>
-                                        <td>{item.STT}</td>
-                                        <td>{item.Tênmón}</td>
-                                        <td>{item.Thờigianchín}</td>
-                                        <td>{item.ĐLtổnggram} gram</td>
-                                        <td>{item.Nướcchấm !== null ? (<div className='text-success'> {item.Nướcchấm}</div>) : (<div className='text-danger'>none</div>)}</td>
-                                        <td>{item.Rauănkèm !== null ? (<div className='text-success'> {item.Rauănkèm}</div>) : (<div className='text-danger'>none</div>)}</td>
-                                        <td>{item.GiábánHCM} VND</td>
-                                    </tr>
-                                ))}
-                            </tbody>
-                        </table>
-                    </div>
-                </div>
-            ))}
-        </section>
-    );
+          </div>
+        ))}
+      </div>
+    </section>
+  );
 };
 
 export default ALC;
