a90ec2aa3099c96a83310c3d8d436bfea9d7a05c
Your Name <you@example.com>
2024-02-17 15:21:28 +0700

    added pakage analytics

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
+import { Analytics } from '@vercel/analytics/react';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'bootstrap/dist/js/bootstrap.bundle.js'
 import 'jquery/dist/jquery'
@@ -14,5 +14,6 @@ import 'slick-carousel/slick/slick-theme.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>,
 )
